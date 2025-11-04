19016cbdf5fd82cbb9545f8842179d100c4eb2d8
Andrii Kurdiumov <kant2002@gmail.com>
2025-11-04 17:45:50 +0100

    Initial parsing numbers

--- Program.fs
+++ Program.fs
@@ -1,2 +1,20 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 printfn "Hello from F#"
+
+open FParsec
+
+type AlgebraicExpression =
+    | ANumber of float
+
+
+let str s = pstring s
+let floatBetweenBrackets = str "[" >>. pfloat .>> str "]"
+let number = pfloat |>> ANumber
+
+let test p str =
+    match run p str with
+    | Success(result, _, _)   -> printfn "Success: %A" result
+    | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+
+test number "1.25"
+test floatBetweenBrackets "[1.25]"
