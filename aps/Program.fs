c21c31d03baaf4c92e0ee2c68b160e41ed66b93c
Andrii Kurdiumov <kant2002@gmail.com>
2025-11-10 19:49:32 +0100

    Remove initial test code. No need for it, given I have tests which cover these cases, and will improve it.

--- aps/Program.fs
+++ aps/Program.fs
@@ -3,38 +3,6 @@ open FParsec
 open System
 open System.IO
 
-let test p str =
-    match run p str with
-    | Success(result, _, position) when position.Index = str.Length -> printfn "Success: %A" result
-    | Success(result, _, position) when position.Index <> str.Length ->
-        printfn "Partial Success: %A, unconsumed input: %s" result (str.Substring(int32 position.Index))
-    | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
-    | failure -> printfn "Generic Failure: %A" failure
-
-
-let testProgram p str =
-    let mutable initialPosition = 0
-    let mutable haveToExit = false
-
-    while (not haveToExit) do
-        match runParserOnSubstring p () "test-stream" str initialPosition (str.Length - initialPosition) with
-        | Success(result, _, position) when position.Index = str.Length ->
-            printfn "Success: %A" result
-            haveToExit <- true
-        | Success(result, _, position) when position.Index <> str.Length ->
-            if int position.Index <= initialPosition then
-                haveToExit <- true
-
-            initialPosition <- initialPosition + int position.Index
-            printfn "Partial Success: %A" result
-        | Failure(errorMsg, _, _) ->
-            printfn "Failure: %s" errorMsg
-            haveToExit <- true
-        | failure ->
-            printfn "Generic Failure: %A" failure
-            haveToExit <- true
-
-
 type AlgebraicValue =
     | VEmpty
     | VInt64 of int64
@@ -155,34 +123,6 @@ and interpretProgram context str =
     | failure ->
         printfn "Generic Failure: %A" failure
 
-// test floatBetweenBrackets "[1.25]"
-// test arity "4"
-// test arity "UNDEF"
-// test markDescriptionElement "X(3)"
-// test markDescriptionElement "X(UNDEF)"
-// test markDescriptionElement "comma( 2,  7, \",\")"
-// test markDescriptionElement "comma( 2,  7, \"-->\")"
-// test markDescriptionElement "comma( 2,  7, \"mod\")"
-// test markDescriptionElement "comma( 2,  7, \"else\")"
-
-// test algebraicExpression "R:=rs()x"
-// test algebraicExpression "F(0) = 1,F(1) = 1"
-// test algebraicExpression
-//     """R:=rs(n)(
-//     F(0) = 1,
-//     F(1) = 1,
-//     F(n) = F(n-1)+F(n-2)
-// )
-//     """
-// test statement "x := 1;"
-// test statement "NAME xx;"
-// test statement "MARKS comma(2,3,\"mod\");"
-
-// TODO: Incorrectly parse as list of algebraic expressions
-//testProgram statement "x := 1; NAME xx; MARKS comma(2,3,\"mod\");"
-//
-// interpretProgram "test" "NAME task; task:=1;"
-// interpretProgram "test" ""
 [<EntryPoint>]
 let main (args) =
     match args with
